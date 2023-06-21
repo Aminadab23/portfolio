241a86624cc1f3b7b4f44041f8efa53eb0f1885c
Yeab23 <yeabman32@gmail.com>
2023-06-21 14:35:24 +0300

    updated typos

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -6,7 +6,7 @@ const Contacts = () => {
         <div className='flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full'>
             <div className='pb-8'>
                 <p className='mt-3 text-4xl font-bold inline border-b-4 border-gray-500'>Contact</p>
-                <p className="py-6 ">Submit the form beow to get in touch with me!</p>
+                <p className="py-6 ">Submit the form below to get in touch with me!</p>
             </div>
             <div className='flex justify-center items-center'>
                 <form action='https://getform.io/f/801c78e1-9f52-4771-830f-05a0d40eff22' method='post' className='flex flex-col w-full md:w-1/2 '>
